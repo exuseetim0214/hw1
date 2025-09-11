181a5f52f43985377ff4aeaf1671a943b2ae690f
Tim Zhou <timzhou@usc.edu>
2025-09-11 04:05:18 +0000

    done with part 1

--- test_split.cpp
+++ test_split.cpp
@@ -10,8 +10,37 @@ g++ split.cpp test_split.cpp -o test_split
 */
 
 #include "split.h"
+#include <iostream>
+#include <cstddef>
 
 int main(int argc, char* argv[])
 {
 
+ Node* in = new Node{3, NULL};
+ in = new Node{2,in};
+ in = new Node{1,in};
+
+ Node* odds = NULL;
+ Node* evens = NULL;
+
+ split(in, odds, evens);
+
+std::cout <<"Odds: ";
+Node* temp;
+ while(odds!= NULL){
+  temp = odds;
+  std::cout << temp-> value << " ";
+  odds = odds -> next;
+  delete temp;
+ }
+std::cout << std::endl;
+std::cout <<"Evens: ";
+ while(evens!= NULL){
+  temp = evens;
+  std::cout <<  temp-> value << " ";
+  evens = evens -> next;
+  delete temp;
+ }
+std::cout << std::endl;
+return 0;
 }
